b804fde083c643c97c242dd55cdafc845daa405e
Harjot Grewal <harjotgrewal@Harjots-MacBook-Pro.local>
2025-09-23 18:25:18 -0700

    Updated driver class with execution time calculated. Fixed

--- src/assignment1/QualityCheck.java
+++ src/assignment1/QualityCheck.java
@@ -7,29 +7,36 @@ import java.io.IOException;
 
 public class QualityCheck {
 	public static void main(String[] args) throws FileNotFoundException, IOException{
-		long startTime = System.nanoTime();
 		Scanner sc = new Scanner(System.in);
 		int choice;
 		ArrayList<Employee> list = LoadEmployees.loadEmployees();
 		System.out.println("If you would like the list to be sorted by name please enter '1' or if you would like by income/pay please enter 2:" );
 		choice = sc.nextInt();
-		if(choice ==1) {
+		if(choice == 1) {
+			double startTime = System.nanoTime();
 			Collections.sort(list, Employee::compareByName);
+			double endTime = System.nanoTime();
+			double duration = (endTime - startTime)/1000000000;
 			System.out.println("Employees sorted by name below: ");
 			for(Employee x : list)
 				System.out.println(x.employeeName + " " + x.employeeID + " " + x.employeePay);
+			
+			System.out.println("Time taken: " + duration + " seconds");
+
 		}
 		else if(choice == 2) {
+			double startTime = System.nanoTime();
 		Collections.sort(list,Employee::compareTo);
+		double endTime = System.nanoTime();
+		double duration = (endTime - startTime)/1000000000;
 		System.out.println("Employees are sorted by pay/income below: ");
 		for(Employee x : list)
 			System.out.println(x.employeeName + " " + x.employeeID + " " + x.employeePay);
+		System.out.println("Time taken: " + duration + " seconds");
+
 		}else{
 			System.out.println("Error, please enter a correct choice.");
 			}
-		sc.close();
-		long endTime = System.nanoTime();
-		long duration = (endTime - startTime)/1000000;
-		System.out.println("Time taken: " + duration + " miliseconds");
+		}
+		
 	}
-}
